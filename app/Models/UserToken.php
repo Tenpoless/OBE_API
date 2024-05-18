d66c3b8c2d5a09fb6eef1d060b93460e78f4fb80
YannIyann <11woww90@gmail.com>
2024-05-18 19:38:39 +0700

    .

--- app/Models/UserToken.php
+++ app/Models/UserToken.php
@@ -16,9 +16,35 @@ class UserToken extends Model
      */
     protected $table = 'user_token';
 
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'int';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
     protected $fillable = [
         'email', 'token', 'date_created',
     ];
-
-    public $timestamps = false;
 }
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Login\LoginController;
 
 // Login
-Route::post('login', [LoginController::class, 'login']);
+Route::post('login', [LoginController::class, 'login'])->name('login');
 
 // Logout
-Route::post('logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('logout', [LoginController::class, 'logout'])->middleware('auth:sanctum')->name('logout');
