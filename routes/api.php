0c4143b72340dad463622558da5c1ccf76eb5ad0
YannIyann <11woww90@gmail.com>
2024-05-29 18:25:48 +0700

    .

--- routes/api.php
+++ routes/api.php
@@ -12,18 +12,18 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::middleware('api')->group(function () {
-    // Route untuk menampilkan hanya nama dosen
+    // Route untuk menampilkan hanya nama dosen - SCR2.1
     Route::get('/halaman-utama-nama/{id}', [HalamanUtamaController::class, 'getNamaDosen']);
 
-    // Route untuk menampilkan nama dosen, email, dan nomor telepon
+    // Route untuk menampilkan nama dosen, email, dan nomor telepon - SCR2.2
     Route::get('/halaman-utama-profile/{id}', [HalamanUtamaController::class, 'getProfileDosen']);
     
-    // Route untuk mendapatkan matkul berdasarkan user ID
+    // Route untuk mendapatkan matkul berdasarkan user ID - SCR5.1.1
     Route::get('/evaluasi-mahasiswa/user/{userId}', [EvaluasiMahasiswaController::class, 'getMatkulByUser']);
 
-    // Route untuk mendapatkan mahasiswa berdasarkan dosen ID
+    // Route untuk mendapatkan mahasiswa berdasarkan dosen ID - SCR5.1.2
     Route::get('/mahasiswa-by-dosen/{id}', [EvaluasiMahasiswaDataController::class, 'getMahasiswaByDosen']);
 
-    // Route untuk mendapatkan detail mahasiswa berdasarkan mahasiswa ID
+    // Route untuk mendapatkan detail mahasiswa berdasarkan mahasiswa ID - SCR5.2
     Route::get('/evaluasi-mahasiswa-detail/{id}', [EvaluasiMahasiswaDetailController::class, 'getEvaluasiDetailsByUserId']);
 });
