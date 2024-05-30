d74bb27cc47e31e54d295c0c3417a352a5f0b88b
Abdul Azis <abdulazisnaufal@gmail.com>
2024-05-30 09:13:25 +0700

    merge login_user to scr5_rpsmahasiswa

--- app/Models/DetailRPS.php
+++ app/Models/DetailRPS.php
@@ -4,42 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use app\Models\SubCpmk;
+use app\Models\Matkul;
 
-class DetailRPS extends Model
+class DetailRps extends Model
 {
     use HasFactory;
 
-    protected $table = 'detail_rps';
+    protected $table = "detail_rps";
 
-    protected $primaryKey = 'id_detailrps';
+    protected $foreignKey = "id_detailrps";
 
-    public $timestamps = false;
-
-    /**
-     * fillable
-     *
-     * @var array
-     */
     protected $fillable = [
-        'id_detailrps',
-        'minggu',
-        'id_subcpmk',
-        'indikator',
-        'kriteria',
-        'luring',
-        'daring',
-        'matteri',
-        'bobot',
-        'id_matkul'
+        "minggu",
+        "id_subcpmk",
+        "indikator",
+        "kriteria",
+        "luring",
+        "daring",
+        "materi",
+        "bobot",
+        "id_matkul"
     ];
 
+    // Definisikan relasi ke model Subcpmk
+    public function subcpmk()
+    {
+        return $this->belongsTo(SubCpmk::class, 'id_subcpmk', 'id_subcpmk');
+    }
+    
+    // Definisikan relasi ke model Matkul
     public function matkul()
     {
-        return $this->belongsTo(DetailRPS::class, 'id_matkul', 'id_matkul');
-    }
-
-    public function evaluasi_mhs()
-    {
-        return $this->belongsTo(EvaluasiMhs::class, 'id_detailrps', 'id_detailrps');
+        return $this->belongsTo(Matkul::class, 'id_matkul', 'id_matkul');
     }
 }
--- app/Models/Subcpmk.php
+++ app/Models/Subcpmk.php
@@ -5,24 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Subcpmk extends Model
+class SubCpmk extends Model
 {
     use HasFactory;
 
-    protected $table = 'subcpmk';
-    protected $primaryKey = 'id_subcpmk';
+    protected $table = "subcpmk";
+
+    protected $primaryKey = "id_subcpmk";
 
     protected $fillable = [
-        'id_subcpmk',
-        'kode_subcpmk',
-        'subcpmk',
-        'kode_baru',
-        'id_cplmk',
-        'id_matkul',
+        "kode_subcpmk",
+        "subcpmk",
+        "kode_baru",
+        "id_cplmk",
+        "id_matkul"
     ];
-
-    public function evaluasi_mhs()
-    {
-        return $this->belongsTo(EvaluasiMhs::class, 'id_subcpmk', 'id_subcpmk');
-    }
 }
